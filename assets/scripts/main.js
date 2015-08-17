cf7d599aaf5e914c99e5ebcac4f74168000d276d
puck3000 <daniel.lerch@gmail.com>
2015-08-17 10:27:35 +0200

    Updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,41 @@
-$(function(){var l=$("#navToggle"),o=$("nav>ul");l.click(function(){o.toggleClass("open")}),smoothScroll.init(),$(".slideshow").flickity({cellAlign:"center",imagesLoaded:!0,pageDots:!1,lazyLoad:!0})});
+$(function() {
+
+// nav toggl stuff
+  var navToggle = $("#navToggle")
+  var nav = $("nav>ul")
+   navToggle.click(function(){
+     nav.toggleClass("open");
+   });
+
+//  init smooth scroll
+  smoothScroll.init();
+
+// flickity
+
+  $('.slideshow').flickity({
+    // options
+    lazyLoad: true,
+    prevNextButtons: false,
+    imagesLoaded: true,
+    wrapAround: true
+  });
+
+  // arrow navi for slideshow
+//
+//   $msg = $("#msg");
+//
+// $(".gallery-cell-image").on('mousemove', function(e) {
+//     var mouseSide;
+//     var $gallery = $('.gallery').flickity();
+//     if ((e.pageX - this.offsetLeft) < $(this).width() / 2) {
+//         mouseSide = 'L';
+//         $(this).addClass("leftHover");
+//     } else {
+//         mouseSide = 'R';
+//         $(this).removeClass("leftHover");
+//     }
+// });
+
+
+// end wrapFunc
+});
