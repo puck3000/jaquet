24cf82fa95edcb63133d360f823b2bf4e451d27e
puck3000 <daniel.lerch@gmail.com>
2015-08-17 16:41:57 +0200

    Updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,29 +12,58 @@ $(function() {
 
 // flickity
 
-  $('.slideshow').flickity({
+var gallery = $('.slideshow').flickity({
     // options
     lazyLoad: true,
     prevNextButtons: false,
+    pageDots: false,
     imagesLoaded: true,
-    wrapAround: true
+    wrapAround: true,
+    autoPlay: 4500
   });
 
   // arrow navi for slideshow
-//
-//   $msg = $("#msg");
-//
-// $(".gallery-cell-image").on('mousemove', function(e) {
-//     var mouseSide;
-//     var $gallery = $('.gallery').flickity();
-//     if ((e.pageX - this.offsetLeft) < $(this).width() / 2) {
-//         mouseSide = 'L';
-//         $(this).addClass("leftHover");
-//     } else {
-//         mouseSide = 'R';
-//         $(this).removeClass("leftHover");
-//     }
-// });
+
+  // display the arrows
+  $msg = $("#msg");
+  $slideshow = $(".slideshow");
+
+$(".slideshow").on('mousemove', function(e) {
+    var mouseSide;
+    if ((e.pageX - this.offsetLeft) < $(this).width() / 2) {
+        mouseSide = 'L';
+        $($slideshow).removeClass("rightHover").addClass("leftHover");
+    } else {
+        mouseSide = 'R';
+        $($slideshow).removeClass("leftHover").addClass("rightHover");
+    }
+});
+
+  // click that fuck
+
+  $('.slideshow').on( 'click', function(e) {
+    var mouseSide;
+    var $gallery = $('.slideshow').flickity({
+        // options
+        lazyLoad: true,
+        prevNextButtons: false,
+        pageDots: false,
+        imagesLoaded: true,
+        wrapAround: true,
+        autoPlay: 4500
+      });
+    if ((e.pageX - this.offsetLeft) < $(this).width() / 2) {
+        mouseSide = 'L';
+        $gallery.flickity('previous');
+    } else {
+        mouseSide = 'R';
+        $gallery.flickity('next');
+    }
+  });
+
+
+
+
 
 
 // end wrapFunc
